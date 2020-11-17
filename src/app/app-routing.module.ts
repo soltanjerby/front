5eccd4d34e0e81d6f6498cde8dd97301e78a7bf6
Jerbi Soltane <jerbi.soltane@insat.u-carthage.tn>
2020-11-17 20:59:57 +0100

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+import { TestComponent } from './test/test.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: TestComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
